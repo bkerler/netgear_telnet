35b957591dafa488dcab47b5d49208b63afb64d3
info@revskills.de <info@revskills.de>
2022-10-23 00:24:17 +0200

    Fix algo after re-emulation

--- telnet-enable2.py
+++ telnet-enable2.py
@@ -484,8 +484,7 @@ def ByteSwap(data):
     a.byteswap()
     return a.tobytes()
 
-
-def genhash1(mac, username, password=''):
+def genhash(mac, username, password='', mode=1):
     # eventually reformat mac
     mac = mac.replace(':', '').upper()
 
@@ -496,37 +495,8 @@ def genhash1(mac, username, password=''):
     assert (len(username) <= 0x10)
     just_username = username.encode('utf8').ljust(0x10, b'\x00')
 
-    assert (len(password) <= 0x50)
-    just_password = password.encode('utf8').ljust(0x50, b'\x00')
-
-    cleartext = (just_mac + just_username + just_password).ljust(0x70, b'\x00')
-    # print(hexlify(cleartext).decode('utf-8'))
-    md5_key = md5(cleartext[:0x70]).digest()
-    # print("MD5:"+hexlify(md5_key).decode('utf-8'))
-    payload = (md5_key + cleartext).ljust(0xB0, b'\x00')
-    # print("Payload: " + hexlify(payload).decode('utf-8'))
-
-    secret_key = ('AMBIT_TELNET_ENABLE+' + password)[:0x80]
-    # print("Key: "+secret_key)
-
-    cipher = Blowfish(secret_key.encode('utf8'), byte_order="little")
-    rdata = b"".join(cipher.encrypt_ecb(payload))
-    # print(hexlify(rdata).decode('utf-8'))
-    return rdata
-
-def genhash2(mac, username, password=''):
-    # eventually reformat mac
-    mac = mac.replace(':', '').upper()
-
-    # Pad the input correctly
-    assert (len(mac) < 0x10)
-    just_mac = mac.encode('utf8').ljust(0x10, b'\x00')
-
-    assert (len(username) <= 0x10)
-    just_username = username.encode('utf8').ljust(0x10, b'\x00')
-
-    assert (len(username) <= 0x10)
-    hashed_pw = bytes(sha256(password.encode('utf-8')).hexdigest().lower(), 'utf-8').ljust(0x30, b'\x00')
+    assert (len(password) <= 0x10)
+    hashed_pw = password.encode('utf-8').ljust(0x30, b'\x00')
 
     cleartext = (just_mac + just_username + hashed_pw).ljust(0x70, b'\x00')
     # print(hexlify(cleartext).decode('utf-8'))
@@ -534,9 +504,11 @@ def genhash2(mac, username, password=''):
     # print("MD5:"+hexlify(md5_key).decode('utf-8'))
     payload = (md5_key + cleartext[:0x70]).ljust(0xB0, b'\x00')
     # print("Payload: " + hexlify(payload).decode('utf-8'))
-
-    secret_key = ('AMBIT_TELNET_ENABLE+' + password)[:0x80]
-    # print("Key: "+secret_key)
+    if mode==1:
+        spassword = sha256(bytes(password,'utf-8')).hexdigest().lower()
+    elif mode==2:
+        spassword = password
+    secret_key = ('AMBIT_TELNET_ENABLE+' + spassword)[:0x80]
 
     cipher = Blowfish(secret_key.encode('utf8'), byte_order="little")
     rdata = b"".join(cipher.encrypt_ecb(payload))
@@ -544,8 +516,12 @@ def genhash2(mac, username, password=''):
     return rdata
 
 
-def hashtest1(mac, username, password, payload):
-    secret_key = ('AMBIT_TELNET_ENABLE+' + password)[:0x80]
+def hashtest(mac, username, password, payload, mode=1):
+    if mode==1:
+        spassword = sha256(bytes(password,'utf-8')).hexdigest().lower()
+    elif mode==2:
+        spassword = password
+    secret_key = ('AMBIT_TELNET_ENABLE+' + spassword)[:0x80]
     cipher = Blowfish(secret_key.encode('utf8'), byte_order="little")
     rdata = b"".join(cipher.decrypt_ecb(payload))
     md5_key = md5(rdata[0x10:][:0x70]).digest()
@@ -559,25 +535,6 @@ def hashtest1(mac, username, password, payload):
     pwd = rdata[0x30:0x50].rstrip(b"\x00").decode('utf-8')
     if password != pwd:
         print("Error pw digest")
-
-
-def hashtest2(mac, username, password, payload):
-    secret_key = ('AMBIT_TELNET_ENABLE+' + password)[:0x80]
-    cipher = Blowfish(secret_key.encode('utf8'), byte_order="little")
-    rdata = b"".join(cipher.decrypt_ecb(payload))
-    md5_key = md5(rdata[0x10:][:0x70]).digest()
-    if rdata[:0x10] != md5_key:
-        print("Error md5")
-    if mac != rdata[0x10:0x10 + (6 * 2)].decode('utf-8'):
-        print("Error mac")
-    rusername = rdata[0x20:0x30].rstrip(b"\x00").decode('utf-8')
-    if username != rusername[:len(username)]:
-        print("Error username")
-    pwd_digest = rdata[0x30:0x30 + (0x20 * 2)].decode('utf-8')
-    hashed_pw = sha256(password.encode('utf-8')).hexdigest().lower()
-    if hashed_pw != pwd_digest:
-        print("Error pw digest")
-
 
 def sendtelnet(ip, data):
     port = 23
@@ -602,8 +559,12 @@ def sendtelnet(ip, data):
         sys.exit(0)
     else:
         conn.send(data)
+        retval = conn.recvfrom(1024)
         conn.close()
-        return True
+        if b"ACK" in retval:
+            return True
+        else:
+            return False
 
 
 def main():
@@ -616,10 +577,10 @@ def main():
 
     if len(args) > 2:
         ip = args[0]
-        mac = args[1]
-        username = args[2]
+        mac = args[1]       # lan_hwaddr
+        username = args[2]  # http_username
     if len(args) == 4:
-        password = args[3]
+        password = args[3]  # http_passwd
 
     # Check if arguments have been pass in the wrong order, and/or if argument specifiers have been used
     for arg in args:
@@ -703,12 +664,14 @@ def main():
     if not username and len(password) > 0:
         username = "admin"  # Most devices only allows 'admin' as the username
     mac = mac.replace(":", "").replace("-", "").upper()
-    hash = genhash1(mac, username, password)
-    hashtest1(mac,username,password,hash)
+
+    hash = genhash(mac, username, password, mode=1)
+    hashtest(mac, username, password, hash, mode=1)
     if sendtelnet(ip, hash):
         print(f"Done sending pw data to {ip}:23")
-    hash2 = genhash2(mac, username, password)
-    hashtest2(mac,username,password,hash2)
+    hash2 = genhash(mac, username, password, mode=2)
+    print(hash2.hex())
+    hashtest(mac, username, password, hash2, mode=2)
     if sendtelnet(ip, hash2):
         print(f"Done sending new pw data to {ip}:23")
 
